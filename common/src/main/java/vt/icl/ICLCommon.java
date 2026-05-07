ce4ffef37c52600b168b43514797cf110041fcb5
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-05-07 21:19:55 +0200

    Update ICLCommon.java

--- common/src/main/java/vt/icl/ICLCommon.java
+++ common/src/main/java/vt/icl/ICLCommon.java
@@ -3,12 +3,11 @@ package vt.icl;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.Holder;
-import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.protocol.game.ClientboundSoundPacket;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.permissions.Permissions;
@@ -17,7 +16,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.level.entity.EntityTypeTest;
-import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vt.icl.config.ConfigManager;
@@ -29,295 +27,155 @@ import vt.icl.permission.PermissionHandler;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import static vt.icl.config.lang.IclTranslationManager.createDefaultTranslationFiles;
 
 public class ICLCommon {
     public static final String MOD_ID = "icl";
-    public static final String MOD_PREFIX = "[" + MOD_ID.toUpperCase() + "] ";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID.toUpperCase());
     public static final Path CONFIG_DIR = new File("./config/" + MOD_ID.substring(0, 1).toUpperCase() + MOD_ID.substring(1)).toPath();
+    
     public static Configuration config = ConfigManager.getConfig();
-    private static Timer TIMER = new Timer(MOD_ID.toUpperCase());
     public static Map<String, String> translations;
     private static Map<String, String> defaultTranslations;
     public static PermissionHandler permissionHandler;
     private static MinecraftServer server;
+    
+    private static long ticksUntilNextClean = -1;
 
     public static void init() {
-        LOGGER.info("Initializing " + MOD_ID.toUpperCase());
+        LOGGER.info("Initializing ICL");
         createDefaultTranslationFiles();
+        reloadTranslations();
+    }
+
+    public static void reloadTranslations() {
         translations = IclTranslationManager.loadTranslation(config.NotificationLang);
         defaultTranslations = IclTranslationManager.loadTranslation("en_us");
     }
 
     public static void onServerStart(MinecraftServer server) {
         ICLCommon.server = server;
+        resetSchedule();
+        LOGGER.info("ICL initialized on server start");
+    }
+
+    public static void resetSchedule() {
         if (config.Delay > 0) {
-            doItemClean(server);
-            if (config.doShowNotification) {
-                setupNotificationTimers(server);
-            }
-            if (config.doNotificationCountdown) {
-                setupCountdownTimer(server);
-            }
+            ticksUntilNextClean = config.Delay * 20L;
         } else {
-            LOGGER.info(MOD_ID.toUpperCase() + " disabled, delay is less than 0");
+            ticksUntilNextClean = -1;
+            LOGGER.info("ICL disabled: delay is 0 or less");
         }
-        LOGGER.info(MOD_ID.toUpperCase() + " initialized");
     }
 
-    public static void onServerStop() {
-        TIMER.cancel();
-        LOGGER.info(MOD_ID.toUpperCase() + " stopped");
-    }
+    public static void onTick(MinecraftServer server) {
+        if (ticksUntilNextClean <= 0) return;
 
-    public static void doItemClean(MinecraftServer server) {
-        long delay = ICLCommon.config.Delay;
-        if (delay < 0) {
-            return;
+        ticksUntilNextClean--;
+
+        long secondsLeft = ticksUntilNextClean / 20;
+        long currentTickInSecond = ticksUntilNextClean % 20;
+
+        if (currentTickInSecond == 0) {
+            handleNotifications(server, secondsLeft);
+            
+            if (secondsLeft <= 0) {
+                clearItems(server);
+                resetSchedule();
+            }
         }
-        ICLCommon.TIMER.schedule(new TimerTask() {
-            @Override
-            public void run() {
-
-                if (ICLCommon.config.doShowNotification) {
-                    setupNotificationTimers(server);
-                }
-                if (ICLCommon.config.doNotificationCountdown) {
-                    setupCountdownTimer(server);
-                }
-
-                server.execute(() -> clearItems(server));
-
-                ICLCommon.TIMER.purge();
-
-                doItemClean(server);
-            }
-        }, delay * 1000);
     }
 
-    public static void setupNotificationTimers(MinecraftServer server) {
-        for (int i = 0; i < ICLCommon.config.NotificationTimes; i++) {
-            int finalI = i;
-            long delay = ICLCommon.config.Delay - ICLCommon.config.NotificationStart + ICLCommon.config.NotificationDelay * i;
-            if (delay < 0 || delay > ICLCommon.config.Delay) {
-                continue;
-            }
-            ICLCommon.TIMER.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    ICLCommon.LOGGER.info("{} seconds left", "Clearing items " + (ICLCommon.config.NotificationStart - ICLCommon.config.NotificationDelay * finalI));
-                    for (var player : server.getPlayerList().getPlayers()) {
-                        MutableComponent message = Component.literal(ICLCommon.MOD_PREFIX + IclTranslate("text.icl.notification", (ICLCommon.config.NotificationStart - ICLCommon.config.NotificationDelay * finalI)) + " ")
-                                .withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor));
-                        IclMessage(player, message);
-                        try {
-                            if (ICLCommon.config.doNotificationSound) {
-                                IclPlaysound(player, false);
-                            }
-                        } catch (Exception e) {
-                            player.sendSystemMessage(Component.literal(e.getMessage()).withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor)));
-                            ICLCommon.LOGGER.error("Failed to play sound: " + e.getMessage());
-                        }
-                    }
+    private static void handleNotifications(MinecraftServer server, long secondsLeft) {
+        // Reguliere notificaties
+        if (config.doShowNotification) {
+            for (int i = 0; i < config.NotificationTimes; i++) {
+                long notifyAt = config.NotificationStart - (long) i * config.NotificationDelay;
+                if (secondsLeft == notifyAt && notifyAt > 0) {
+                    broadcastIclMessage(server, "text.icl.notification", true, secondsLeft);
                 }
-            }, delay * 1000);
+            }
+        }
+
+        if (config.doNotificationCountdown && secondsLeft <= config.CountdownStart && secondsLeft > 0) {
+            broadcastIclMessage(server, "text.icl.countdown", false, secondsLeft);
+        }
+    }
+
+    private static void broadcastIclMessage(MinecraftServer server, String translationKey, boolean playSound, Object... args) {
+        for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+            MutableComponent message = Component.literal(IclTranslate(translationKey, args))
+                    .withStyle(ChatFormatting.valueOf(config.NotificationColor));
+            
+            IclMessage(player, message);
+            
+            if (playSound && config.doNotificationSound) {
+                IclPlaysound(player, false);
+            }
         }
     }
 
     private static void IclMessage(ServerPlayer player, MutableComponent message) {
         if (permissionCheckforCancel(player.createCommandSourceStack())) {
-            message.append(Component.literal(IclTranslate("text.icl.cancel.button"))
-                    .withStyle(style -> style.withClickEvent(IclCancelEvent()))
+            message.append(Component.literal(" " + IclTranslate("text.icl.cancel.button"))
+                    .withStyle(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/icl cancel")))
                     .withStyle(ChatFormatting.RED));
         }
         player.sendSystemMessage(message);
     }
 
-    public static void setupCountdownTimer(MinecraftServer server) {
-        long countdownstart = ICLCommon.config.CountdownStart;
-        if (countdownstart > ICLCommon.config.Delay) {
-            countdownstart = ICLCommon.config.Delay;
-        }
-        long delay = ICLCommon.config.Delay - countdownstart;
-        if (delay < 0 || delay > ICLCommon.config.Delay) {
-            return;
-        }
-        if (countdownstart < 0) {
-            return;
-        }
-
-
-        long finalCountdownstart = countdownstart;
-        ICLCommon.TIMER.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                for (int i = 0; i < finalCountdownstart; i++) {
-                    int finalI = i;
-                    ICLCommon.TIMER.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            ICLCommon.LOGGER.info("{} seconds left", "Clearing items " + (finalCountdownstart - finalI));
-                            for (var player : server.getPlayerList().getPlayers()) {
-                                MutableComponent message = Component.literal(ICLCommon.MOD_PREFIX + IclTranslate("text.icl.countdown", (finalCountdownstart - finalI)) + " ")
-                                        .withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor));
-                                IclMessage(player, message);
-                            }
-                        }
-                    }, finalI * 1000L);
-                }
-            }
-        }, (delay) * 1000);
-    }
-
     public static void clearItems(MinecraftServer server) {
-        ICLCommon.LOGGER.info("Clearing items");
-        for (var player : server.getPlayerList().getPlayers()) {
-            if (ICLCommon.config.doShowNotification) {
-                player.sendSystemMessage(Component.literal(ICLCommon.MOD_PREFIX + IclTranslate("text.icl.clear")).withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor)));
-                try {
-                    if (ICLCommon.config.doLastNotificationSound) {
-                        IclPlaysound(player, true);
-                    }
-                } catch (Exception e) {
-                    player.sendSystemMessage(Component.literal(e.getMessage()).withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor)));
-                    ICLCommon.LOGGER.error("Failed to play sound: " + e.getMessage());
-                }
-            }
-        }
         int count = 0;
         for (var world : server.getAllLevels()) {
             for (var entity : world.getEntities(EntityTypeTest.forClass(ItemEntity.class), Entity::isAlive)) {
-                if (ICLCommon.config.preserveNoPickupItems) {
-                    ItemEntityAccessor accessor = (ItemEntityAccessor) entity;
-                    if (accessor.getPickupDelay() == Short.MAX_VALUE) {
-                        continue;
-                    }
-                }
-                if (ICLCommon.config.preserveNoDespawnItems) {
-                    if (entity.getAge() == Short.MIN_VALUE) {
-                        continue;
-                    }
-                }
+                if (config.preserveNoPickupItems && ((ItemEntityAccessor) entity).getPickupDelay() == Short.MAX_VALUE) continue;
+                if (config.preserveNoDespawnItems && entity.getAge() == Short.MIN_VALUE) continue;
+                
                 count += entity.getItem().getCount();
-                entity.remove(Entity.RemovalReason.DISCARDED);
+                entity.discard();
             }
         }
-        for (var player : server.getPlayerList().getPlayers()) {
-            if (ICLCommon.config.doShowNotification) {
-                player.sendSystemMessage(Component.literal(ICLCommon.MOD_PREFIX + IclTranslate("text.icl.clear.finish", count)).withStyle(ChatFormatting.valueOf(ICLCommon.config.NotificationColor)));
+
+        for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+            if (config.doShowNotification) {
+                player.sendSystemMessage(Component.literal(IclTranslate("text.icl.clear.finish", count))
+                        .withStyle(ChatFormatting.valueOf(config.NotificationColor)));
+                if (config.doLastNotificationSound) IclPlaysound(player, true);
             }
         }
-        ICLCommon.LOGGER.info("Items cleared: {}", count);
+        LOGGER.info("Items cleared: {}", count);
     }
 
     public static void reloadIcl() {
-        ICLCommon.TIMER.cancel();
-        ICLCommon.TIMER = new Timer(ICLCommon.MOD_ID.toUpperCase());
-        ICLCommon.config = ConfigManager.getConfig();
-        if (ICLCommon.config.Delay > 0) {
-            doItemClean(ICLCommon.server);
-            if (ICLCommon.config.doShowNotification) {
-                setupNotificationTimers(ICLCommon.server);
-            }
-            if (ICLCommon.config.doNotificationCountdown) {
-                setupCountdownTimer(ICLCommon.server);
-            }
-        } else {
-            ICLCommon.LOGGER.info(ICLCommon.MOD_ID.toUpperCase() + " disabled, delay is less than 0");
-        }
+        config = ConfigManager.getConfig();
+        reloadTranslations();
+        resetSchedule();
+        LOGGER.info("ICL Config reloaded");
     }
-
-    public static void CancelIcl(int tempDelay) {
-        ICLCommon.TIMER.cancel();
-        ICLCommon.TIMER = new Timer(ICLCommon.MOD_ID.toUpperCase());
-        if (tempDelay > 0) {
-            ICLCommon.TIMER.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    ICLCommon.config = ConfigManager.getConfig();
-                    if (ICLCommon.config.Delay > 0) {
-                        doItemClean(ICLCommon.server);
-                        if (ICLCommon.config.doShowNotification) {
-                            setupNotificationTimers(ICLCommon.server);
-                        }
-                        if (ICLCommon.config.doNotificationCountdown) {
-                            setupCountdownTimer(ICLCommon.server);
-                        }
-                    } else {
-                        ICLCommon.LOGGER.info(ICLCommon.MOD_ID.toUpperCase() + " disabled, delay is less than 0");
-                    }
-                }
-            }, tempDelay * 1000L);
-        } else {
-            ICLCommon.config = ConfigManager.getConfig();
-            if (ICLCommon.config.Delay > 0) {
-                doItemClean(ICLCommon.server);
-                if (ICLCommon.config.doShowNotification) {
-                    setupNotificationTimers(ICLCommon.server);
-                }
-                if (ICLCommon.config.doNotificationCountdown) {
-                    setupCountdownTimer(ICLCommon.server);
-                }
-            } else {
-                ICLCommon.LOGGER.info(ICLCommon.MOD_ID.toUpperCase() + " disabled, delay is less than 0");
-            }
-        }
-    }
-
-    public static ClickEvent IclCancelEvent() {
-        return new ClickEvent.RunCommand("/icl cancel");
-    }
-
 
     public static String IclTranslate(String key, Object... args) {
-        String translation = null;
-        if (ICLCommon.translations != null) {
-            translation = ICLCommon.translations.get(key);
-            ICLCommon.LOGGER.debug("Translation: {}", translation);
-        }
-        if (translation == null && ICLCommon.defaultTranslations != null) {
-            translation = ICLCommon.defaultTranslations.get(key);
-            ICLCommon.LOGGER.debug("Default Translation: {}", translation);
-        }
+        String translation = (translations != null) ? translations.get(key) : null;
+        if (translation == null && defaultTranslations != null) translation = defaultTranslations.get(key);
+        
         if (translation != null) {
-            if (args != null && args.length > 0) {
-                return String.format(translation, args);
-            } else {
-                return translation;
-            }
-        } else {
-            return key;
+            return args.length > 0 ? String.format(translation, args) : translation;
         }
+        return key;
     }
 
     public static void IclPlaysound(ServerPlayer player, boolean isLastSound) {
-        Vec3 vec3d;
-        double e = player.getX();
-        double f = player.getY();
-        double g = player.getZ();
-        double h = e * e + f * f + g * g;
-        double k = Math.sqrt(h);
-        vec3d = new Vec3(player.getX() + e / k * 2.0, player.getY() + f / k * 2.0, player.getZ() + g / k * 2.0);
-        Identifier sound;
-        if (isLastSound) {
-            sound = Identifier.parse(ICLCommon.config.LastNotificationSound);
-        } else {
-            sound = Identifier.parse(ICLCommon.config.NotificationSound);
-        }
+        ResourceLocation sound = ResourceLocation.parse(isLastSound ? config.LastNotificationSound : config.NotificationSound);
         Holder<SoundEvent> registryEntry = Holder.direct(SoundEvent.createVariableRangeEvent(sound));
+        
         player.connection.send(new ClientboundSoundPacket(registryEntry,
-                SoundSource.PLAYERS, vec3d.x(), vec3d.y(), vec3d.z(), 1, 1, 1), null);
+                SoundSource.PLAYERS, player.getX(), player.getY(), player.getZ(), 1.0f, 1.0f, server.overworld().getRandom().nextLong()), null);
     }
 
     private static boolean permissionCheckforCancel(CommandSourceStack source) {
-        if (ICLCommon.permissionHandler != null) {
-            return ICLCommon.permissionHandler.hasPermission(source, ICLCommon.MOD_ID + "." + "cancel");
-        } else {
-            return !ICLCommon.config.RequireOpCancel || source.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER);
+        if (permissionHandler != null) {
+            return permissionHandler.hasPermission(source, MOD_ID + ".cancel");
         }
+        return !config.RequireOpCancel || source.hasPermission(Permissions.COMMANDS_GAMEMASTER);
     }
 }
